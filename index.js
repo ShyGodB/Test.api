7d87560b551a1c777114bacd1eebb07048fc9161
qbfang@163.com <qbfang@163.com>
2022-11-05 14:55:13 +0800

    1

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static('./app'));
-const port = 8090;
-app.listen(port, () => {
-    console.log(`Server is running at ${port}`)
-});
